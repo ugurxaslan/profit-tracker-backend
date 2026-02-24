38ee12c93845e31a6e7d5d594fb84f433978de29
Uğur Aslan <ugurxaslan@gmail.com>
2026-02-25 00:47:58 +0300

    base entity

---
+++ src/main/java/com/ugurxaslan/profit_tracker_backend/model/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.ugurxaslan.profit_tracker_backend.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass // bu entityden tablo oluşmaması için sadece miras için kullanacağız
+@EntityListeners(AuditingEntityListener.class) // tarihleri otomatik dolduracak
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
