fbadafe2c4cf178eca1881453c7a5001a9d45fcf
Uğur Aslan <ugurxaslan@gmail.com>
2026-02-24 11:55:44 +0300

    Initial commit: Spring Boot project structure

---
+++ src/main/java/com/ugurxaslan/profit_tracker_backend/ProfitTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ugurxaslan.profit_tracker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfitTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfitTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ugurxaslan/profit_tracker_backend/ProfitTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugurxaslan.profit_tracker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfitTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
