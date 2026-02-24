f8fb90ee560e29e9916e56bc31b897dc6c609f14
Uğur Aslan <ugurxaslan@gmail.com>
2026-02-25 00:47:45 +0300

    user enums

---
+++ src/main/java/com/ugurxaslan/profit_tracker_backend/enums/UserRole.java
@@ -0,0 +1,7 @@
+package com.ugurxaslan.profit_tracker_backend.enums;
+
+
+public enum UserRole {
+    USER,
+    ADMIN;
+}
---
+++ src/main/java/com/ugurxaslan/profit_tracker_backend/enums/UserStatus.java
@@ -0,0 +1,8 @@
+package com.ugurxaslan.profit_tracker_backend.enums;
+
+public enum UserStatus {
+    ACTIVE,
+    INACTIVE,
+    BANNED,
+    DELETED;
+}
