3df585f7a4f5269c83bb87aff612826c5804355c
Uğur Aslan <ugurxaslan@gmail.com>
2026-03-03 14:42:55 +0300

    library editing

--- src/main/java/com/ugurxaslan/profit_tracker_backend/model/Transaction.java
+++ src/main/java/com/ugurxaslan/profit_tracker_backend/model/Transaction.java
@@ -1,60 +1,49 @@
-package com.ugurxaslan.profit_tracker_backend.model;
+    package com.ugurxaslan.profit_tracker_backend.model;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
+    import java.math.BigDecimal;
+    import java.time.LocalDateTime;
 
-import com.ugurxaslan.profit_tracker_backend.enums.TransactionType;
+    import com.ugurxaslan.profit_tracker_backend.enums.TransactionType;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+    import jakarta.persistence.*;
+    import lombok.*;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@Entity
-@Table(name = "transactions")
-public class Transaction extends BaseEntity {
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    @Entity
+    @Table(name = "transactions")
+    public class Transaction extends BaseEntity {
     
     @Enumerated(EnumType.STRING)
     @Column(name = "type", nullable = false)
     private TransactionType transactionType;
 
-    @Column(name = "quantity", nullable = false, precision = 19, scale = 8,updatable = false)
-    private BigDecimal quantity;
+        @Column(name = "quantity", nullable = false, precision = 19, scale = 8,updatable = false)
+        private BigDecimal quantity;    
 
-    @Column(name = "unit_cost", nullable = false, precision = 19, scale =2,updatable = false)
-    private BigDecimal unitCost;
+        @Column(name = "unit_cost", nullable = false, precision = 19, scale =2,updatable = false)
+        private BigDecimal unitCost;
 
-    @Column(name = "total_cost", nullable = false, precision = 19, scale = 2,updatable = false)
-    private BigDecimal totalCost;
+        @Column(name = "total_cost", nullable = false, precision = 19, scale = 2,updatable = false)
+        private BigDecimal totalCost;
 
-    @Column(name = "fee", precision = 19, scale = 2,updatable = false)
-    private BigDecimal fee;
+        @Column(name = "fee", precision = 19, scale = 2)
+        private BigDecimal fee;
 
-    @Column(name = "transaction_date", nullable = false)
-    private LocalDateTime transactionDate;
+        @Column(name = "transaction_date", nullable = false)
+        private LocalDateTime transactionDate;
 
-    // Relationships
+        // Relationships
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "wallet_id", nullable = false)
-    private Wallet wallet;
+        @ManyToOne(fetch = FetchType.LAZY, optional = false)
+        @JoinColumn(name = "wallet_id", nullable = false)
+        private Wallet wallet;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "asset_id", nullable = false)
-    private Asset asset;
+        @ManyToOne(fetch = FetchType.LAZY, optional = false)
+        @JoinColumn(name = "asset_id", nullable = false)
+        private Asset asset;
 
-}
+    }
