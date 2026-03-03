67b1263617490a900450ede9b2684559b1aa2797
Uğur Aslan <ugurxaslan@gmail.com>
2026-03-03 14:43:29 +0300

    transient field editing

--- src/main/java/com/ugurxaslan/profit_tracker_backend/model/Wallet.java
+++ src/main/java/com/ugurxaslan/profit_tracker_backend/model/Wallet.java
@@ -1,22 +1,9 @@
 package com.ugurxaslan.profit_tracker_backend.model;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Getter
 @Setter
@@ -31,23 +18,21 @@ public class Wallet extends BaseEntity {
     @Column(name = "wallet_name", nullable = false)
     private String walletName = "Default Wallet";
 
+    @Transient
     @Builder.Default
-    @Column(name = "cash", nullable = false, precision = 19, scale = 2)
     private BigDecimal cash = BigDecimal.ZERO;
 
+    @Transient
     @Builder.Default
-    @Column(name = "portfolio_value", nullable = false, precision = 19, scale = 2)
     private BigDecimal portfolioValue = BigDecimal.ZERO;
+
+    @Transient
+    @Builder.Default
+    private BigDecimal totalValue = BigDecimal.ZERO;
 
     // Relationships
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "user_id", nullable = false, updatable = false)
     private User user;
-
-    @Builder.Default
-    @OneToMany(mappedBy = "wallet", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
-    private List<WalletAsset> assetList = new ArrayList<>();
-
-
 }
