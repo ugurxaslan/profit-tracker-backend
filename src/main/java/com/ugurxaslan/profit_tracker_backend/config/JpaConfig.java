b493b7a0c53d69823e78cefcc0721e21b823d5d1
Uğur Aslan <ugurxaslan@gmail.com>
2026-03-09 00:49:57 +0300

    remove blank line

--- src/main/java/com/ugurxaslan/profit_tracker_backend/config/JpaConfig.java
+++ src/main/java/com/ugurxaslan/profit_tracker_backend/config/JpaConfig.java
@@ -3,7 +3,6 @@ package com.ugurxaslan.profit_tracker_backend.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-
 @Configuration
 @EnableJpaAuditing
 public class JpaConfig {
