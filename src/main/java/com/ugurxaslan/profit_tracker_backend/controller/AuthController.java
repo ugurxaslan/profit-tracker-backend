d266e524db2c8e4f730db37b5463c160a381ad4f
Uğur Aslan <ugurxaslan@gmail.com>
2026-03-12 00:04:47 +0300

    implement authentication service and controller

---
+++ src/main/java/com/ugurxaslan/profit_tracker_backend/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.ugurxaslan.profit_tracker_backend.controller;
+
+import com.ugurxaslan.profit_tracker_backend.dto.request.LoginRequestDTO;
+import com.ugurxaslan.profit_tracker_backend.dto.response.LoginResponseDTO;
+import com.ugurxaslan.profit_tracker_backend.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@Valid @RequestBody LoginRequestDTO requestDTO) {
+        return ResponseEntity.ok(authService.login(requestDTO));
+    }
+}
