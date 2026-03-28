36742e0fe5157343c45edc16604bbe7d753de594
Uğur Aslan <ugurxaslan@gmail.com>
2026-03-28 16:37:20 +0300

    add security definitions to OpenAPI configuration for JWT authentication

--- src/main/java/com/ugurxaslan/profit_tracker_backend/config/OpenApiConfig.java
+++ src/main/java/com/ugurxaslan/profit_tracker_backend/config/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package com.ugurxaslan.profit_tracker_backend.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +17,14 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("Profit Tracker API")
                         .version("1.0")
-                        .description("Kâr ve Zarar Takip Sistemi Backend Dokümantasyonu"));
+                        .description("Kâr ve Zarar Takip Sistemi Backend Dokümantasyonu"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("JWT token ile giriş yapınız")));
     }
 }
