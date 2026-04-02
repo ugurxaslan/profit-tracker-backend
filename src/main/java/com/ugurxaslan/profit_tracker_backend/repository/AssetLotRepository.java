d9907552dd690773dd466e6ae369262951f279fa
Uğur Aslan <ugurxaslan@gmail.com>
2026-04-02 16:05:07 +0300

    refactor AssetLotRepository

--- src/main/java/com/ugurxaslan/profit_tracker_backend/repository/AssetLotRepository.java
+++ src/main/java/com/ugurxaslan/profit_tracker_backend/repository/AssetLotRepository.java
@@ -1,6 +1,7 @@
 package com.ugurxaslan.profit_tracker_backend.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,9 +11,9 @@ import com.ugurxaslan.profit_tracker_backend.model.AssetLot;
 @Repository
 public interface AssetLotRepository extends JpaRepository<AssetLot, Long> {
 
-	List<AssetLot> findByWalletAsset_IdOrderByTransaction_TransactionDateAsc(Long walletAssetId);
+	List<AssetLot> findByWalletAsset_IdAndIsClosedFalseOrderByTransaction_TransactionDateAsc(
+			Long walletAssetId);
 
-	List<AssetLot> findByWalletAsset_IdAndIsClosedFalseOrderByTransaction_TransactionDateAsc(Long walletAssetId);
+	Optional<AssetLot> findByTransaction_Id(Long transactionId);
 
-	List<AssetLot> findByWalletAsset_Wallet_IdAndAsset_IdAndIsClosedFalseOrderByTransaction_TransactionDateAsc(Long walletId, Long assetId);
 }
